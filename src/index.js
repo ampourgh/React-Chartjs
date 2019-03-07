9e5d9d6ddd21aecd93493142cbc2ccd5980ded14
Emerson Pourghaed <Emerson.Pourghaed@txdmv.gov>
2019-03-07 13:13:50 -0600

    feat: separate app for other graph
    instead of having all the graphs in app.js, put in separate app so that the graph could place elsewhere on index.html, without having to put too much content in react render.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import App2 from './App2';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(<App2 />, document.getElementById('root2'));
