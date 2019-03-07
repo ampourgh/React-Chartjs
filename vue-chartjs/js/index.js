ab0cb76c744097de210a40194bae24fa7fc6bb7a
Emerson Pourghaed <Emerson.Pourghaed@txdmv.gov>
2019-03-07 15:32:59 -0600

    vue chartjs bar graph

---
+++ vue-chartjs/js/index.js
@@ -0,0 +1,52 @@
+
+Vue.component('line-chart', {
+  extends: VueChartJs.Bar,
+  props: ['data', 'options'],
+  mounted () {
+    this.renderChart({
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [
+        {
+          label: 'Analytics Per Month',
+          backgroundColor: '#f87979',
+          data: [40, 39, 10, 40, 39, 80, 40, 55, 20, 40, 35, 45],
+          backgroundColor:[
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+            'rgba(153, 102, 255, 0.6)',
+            'rgba(255, 159, 64, 0.6)',
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)'
+          ],
+          borderWidth: 1
+        }
+      ]
+    }, {responsive: true, maintainAspectRatio: false})
+  }
+
+})
+
+var vm = new Vue({
+  el: '.app',
+  data: {
+    message: 'Vue.js render of Graphjs'
+  }
+})
